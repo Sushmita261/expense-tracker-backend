39de3e98c39371636f6b926e3b07396d3286f82c
Sushmita Kumari <sushmitakumarisingh2@gmail.com>
2025-07-07 14:00:26 +0530

    Initial commit - Spring Boot backend setup

---
+++ src/main/java/com/expensetracker/expensebackend/ExpensebackendApplication.java
@@ -0,0 +1,13 @@
+package com.expensetracker.expensebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expensetracker/expensebackend/ExpensebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensetracker.expensebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
